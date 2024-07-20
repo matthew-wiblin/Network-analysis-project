a9c5f936021f4b15cbe3d67f7fc7f86c030dde03
Matthew Wiblin <matthew@wibles.net>
2024-07-20 21:29:19 +0100

    added port scanning

--- scripts/get_ping_info.py
+++ scripts/get_ping_info.py
@@ -4,6 +4,7 @@ def get_ping_connectivity():
     result = subprocess.run(["ping", "-c", "5", "google.com"], capture_output=True, text=True)
     ping_times = []
     packet_loss = 0
+
     for line in result.stdout.splitlines():
         if "time=" in line:
             parts = line.split()
@@ -13,10 +14,12 @@ def get_ping_connectivity():
         if "packet loss" in line:
             parts = line.split("%")
             packet_loss = parts[0].strip().split()[-1]
+    
     total_ping_time = 0
     for str in ping_times:
         total_ping_time += float(str)
     ave_ping_time = total_ping_time / 5
+
     ave_ping_status = ""
     if ave_ping_time < 30:
         ave_ping_status = "Good"
@@ -24,6 +27,7 @@ def get_ping_connectivity():
         ave_ping_status = "Average"
     else:
         ave_ping_status = "Slow"
+
     print("Check connectivity:")
     print("-- Command = 'ping -c 5 google.com'")
     print(f"-- {ave_ping_status} ping speed, average ping time = {round(ave_ping_time, 3)} ms, packet loss = {packet_loss}%\n")
---
+++ scripts/port_scanning.py
@@ -0,0 +1,29 @@
+import socket
+
+def port_scanning():
+
+    successful = []
+    connection_refused = []
+    unsuccesful = []
+    ports = [22, 25, 53, 80, 110, 143, 443, 3306, 5000, 8080]
+
+    for port in ports:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2)
+        try:
+            s.connect(("scanme.nmap.org", port))
+            successful.append(port)
+            s.close()
+        except ConnectionRefusedError:
+            connection_refused.append(port)
+        except:
+            unsuccesful.append(port)
+    
+    print("TCP port connection attempts:")
+    print("-- Command = Socket connection attempts")
+    print(f"-- Successful port connections = {successful}")
+    print(f"-- Connection refused = {connection_refused}")
+    print(f"-- Unsuccessful port connections = {unsuccesful}\n")
+
+if __name__ == "__main__":
+    port_scanning()
