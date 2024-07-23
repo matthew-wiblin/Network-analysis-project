e422d9850bb76c7f07f6af25ee717a28bc972ec5
Matthew Wiblin <matthew@wibles.net>
2024-07-23 09:03:08 +0100

    small changes

--- cpp-packet-analyser/main.cpp
+++ cpp-packet-analyser/main.cpp
@@ -7,20 +7,17 @@ int main() {
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t *handle;
 
-    // Open the network device for packet capture
     handle = pcap_open_live("enX0", BUFSIZ, 1, 1000, errbuf);
     if (handle == nullptr) {
         std::cerr << "Failed to open device: " << errbuf << std::endl;
         return 1;
     }
 
-    // Start capturing packets
     if (pcap_loop(handle, 10, packet_handler, nullptr) < 0) {
         std::cerr << "Error during packet capture: " << pcap_geterr(handle) << std::endl;
         return 1;
     }
 
-    // Close the capture handle
     pcap_close(handle);
 
     return 0;
--- cpp-packet-analyser/packet_handler.cpp
+++ cpp-packet-analyser/packet_handler.cpp
@@ -6,15 +6,13 @@
 #include "header.h"
 
 void packet_handler(u_char *user_data, const struct pcap_pkthdr *pkthdr, const u_char *packet) {
-    // Pointer to the IP header
-    const struct ip *iph = (struct ip*)(packet + 14); // Skip Ethernet header (14 bytes)
 
-    // Print basic packet information
+    const struct ip *iph = (struct ip*)(packet + 14);
+
     std::cout << "Packet received, length: " << pkthdr->len << std::endl;
     std::cout << "Source IP: " << inet_ntoa(iph->ip_src) << std::endl;
     std::cout << "Destination IP: " << inet_ntoa(iph->ip_dst) << std::endl;
 
-    // Check the protocol
     switch (iph->ip_p) {
         case IPPROTO_TCP: {
             std::cout << "Protocol: TCP" << std::endl; break;
